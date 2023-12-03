f8adbdaa5b20e63a9067d2b9ef63de2d73af35d5
Muflone <>
2023-12-04 00:42:15 +0100

    Initial commit

---
+++ nextcloud_upgrade.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+for action in "upgrade" "db:add-missing-columns" "db:add-missing-indices" "db:add-missing-primary-keys" "db:convert-filecache-bigint" "maintenance:mode --off"
+do
+  docker-compose exec -u 33 nextcloud php occ $action
+done
+bash nextcloud_cron.sh
